37f5cd8f689ed1560f00fb5d3d8c2bf5885a52de
TinaHeiligers <tina.heiligers@gmail.com>
2019-07-28 08:00:50 -0700

    Adds very basic nav and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import '@elastic/eui/dist/eui_theme_light.css';
-import { AppLayout } from './AppLayout';
+// import { AppLayout } from './AppLayout';
+function Index() {
+  return <h1 style={{ fontWeight: 'bold' }}>Home</h1>;
+}
 
+function About() {
+  return <h1 style={{ fontWeight: 'bold' }}>About</h1>;
+}
+
+function Users() {
+  return <h1 style={{ fontWeight: 'bold' }}>Users</h1>;
+}
 function App() {
-
   return (
     <div className="App">
-      <AppLayout />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <hr />
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
     </div>
   );
 }
