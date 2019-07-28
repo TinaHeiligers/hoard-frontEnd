a15c89eda95f62535b0f817c970d17d2c44b493c
TinaHeiligers <tina.heiligers@gmail.com>
2019-07-28 10:59:24 -0700

    routing working without Eui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 import '@elastic/eui/dist/eui_theme_light.css';
 // import { AppLayout } from './AppLayout';
-function Index() {
+function Index({ ...routeProps }) {
   return <h1 style={{ fontWeight: 'bold' }}>Home</h1>;
 }
 
-function About() {
+function About({ ...routeProps }) {
   return <h1 style={{ fontWeight: 'bold' }}>About</h1>;
 }
 
-function Users() {
+function Users({ ...routeProps }) {
   return <h1 style={{ fontWeight: 'bold' }}>Users</h1>;
+}
+
+function NoMatch() {
+  return <h1>404</h1>;
+}
+
+function Something({ ...routeProps }) {
+  return <h1>Something</h1>
+}
+
+const FadingRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={routeProps => (
+    <div>
+      <Component {...routeProps} />
+    </div>
+  )}>
+  </Route>
+)
+
+function OldSchool(props) {
+  return (
+    <Fragment>
+      <h1>OldSchool</h1>
+      <h3>Props:</h3>
+      <ul>
+        {Object.keys(props).map(prop => <li style={{ listStyle: "square" }}>{prop}</li>)}
+      </ul>
+    </Fragment >
+  )
+}
+
+function OldSchoolMenuLink({ label, to, activeOnlyWhenExact }) {
+  return (
+    <Route
+      path={to}
+      exact={activeOnlyWhenExact}
+      children={({ match }) => (
+        <div className={match ? "selected" : ""}>
+          {match ? "---> " : ""}
+          <Link to={to}>{label}</Link>
+        </div>
+      )}
+    />
+  );
 }
 function App() {
   return (
@@ -23,18 +67,35 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about/">About</Link>
+            <NavLink
+              to="/about/"
+              activeClassName="selected"
+              aria-current="page"
+            >About</NavLink>
           </li>
           <li>
             <Link to="/users/">Users</Link>
+          </li>
+          <li>
+            <Link to="/cool/">Cool</Link>
+          </li>
+          <li>
+            <OldSchoolMenuLink to="/oldschool" label="OldSchool" activeOnlyWhenExact={true} />
           </li>
         </ul>
       </nav>
       <hr />
       <hr />
-      <Route path="/" exact component={Index} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
+      <Switch>
+        {/* Using component to render, the component is returned directly with the router using React.createElement. If using an inline function to the component prop, a new component is rednered everytime instead of the existing one updating.*/}
+        <Route exact path="/" component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+        {/* FadingRoute returns a function that renders props Using a render function to render the component, will be superceeded by <Route component />. Does not create a bew React element */}
+        <FadingRoute path="/cool" component={Something} />
+        <Route path="/oldschool" component={OldSchool} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   );
 }
