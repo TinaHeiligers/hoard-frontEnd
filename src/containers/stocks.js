86d45278d9903fef65b2faa259bb3aa05933e833
TinaHeiligers <tina.heiligers@gmail.com>
2019-08-17 16:29:12 -0700

    hookifies Stocks class

--- src/containers/stocks.js
+++ src/containers/stocks.js
@@ -1,60 +1,74 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import stocksActions from '../redux/stocksActions';
 
 const loadStocksRequest = stocksActions.loadStocksRequest;
 
-const StocksFunctionalComponent = ({ stocks, error }) => (
-  stocks && stocks.length ?
-    <div>
-      <p>We have {stocks.length} stocks.</p>
-      {stocks.map((stock) =>
-        <ol key={stock.id}>
-          {Object.keys(stock).map((item, index) =>
-            <li key={index}>
-              <span>{item}: </span>
-              <span>{stock[item]}</span>
-            </li>
-          )}
-          <br />
-        </ol>
-      )}
-    </div> : <div>Loading...</div>
-)
-
-
-class Stocks extends Component {
-  componentDidMount() {
-    this.props.loadStocksRequest();
+const Stocks = () => {
+  const stocks = useSelector(state => state.stocks.allStocks);
+  const useFetching = (someFetchActionCreator, dispatch) => {
+    useEffect(() => {
+      dispatch(someFetchActionCreator());
+    })
   }
-
-  render() {
-    if (this.props.stocks || this.props.stocks.length) {
-      return (
-        <div>
-          <p>We have {this.props.stocks.length} stocks.</p>
-          {this.props.stocks.map((stock) =>
-            <ol key={stock.id}>
-              {Object.keys(stock).map((item, index) =>
-                <li key={index}>
-                  <span>{item}: </span>
-                  <span>{stock[item]}</span>
-                </li>
-              )}
-              <br />
-            </ol>
-          )}
-        </div>
-      )
-    }
-    return <p>Loading...</p>
+  useFetching(loadStocksRequest, useDispatch());
+  const error = useSelector(state => state.stocks.error)
+  if (error) {
+    return <h1>We have an error: {error}</h1>
+  } else if (stocks && stocks.length) {
+    return (<div>
+      {
+        stocks.map((stock) =>
+          <ol key={stock.id}>
+            {Object.keys(stock).map((item, index) =>
+              <li key={index}>
+                <span>{item}: </span>
+                <span>{stock[item]}</span>
+              </li>
+            )}
+            <br />
+          </ol>
+        )
+      }
+    </div >)
+  } else {
+    return (<div>Loading...</div>)
   }
 }
-export default connect(
-  state => ({
-    stocks: state.stocks.allStocks || [],
-    error: state.stocks.error || null,
-  }),
-  { loadStocksRequest }
-)(Stocks);
+
+export default Stocks
+
+// class Stocks extends Component {
+//   componentDidMount() {
+//     this.props.loadStocksRequest();
+//   }
+
+//   render() {
+//     if (this.props.stocks || this.props.stocks.length) {
+//       return (
+//         <div>
+//           <p>We have {this.props.stocks.length} stocks.</p>
+//           {this.props.stocks.map((stock) =>
+//             <ol key={stock.id}>
+//               {Object.keys(stock).map((item, index) =>
+//                 <li key={index}>
+//                   <span>{item}: </span>
+//                   <span>{stock[item]}</span>
+//                 </li>
+//               )}
+//               <br />
+//             </ol>
+//           )}
+//         </div>
+//       )
+//     }
+//     return <p>Loading...</p>
+//   }
+// }
+// export default connect(
+//   state => ({
+//     stocks: state.stocks.allStocks || [],
+//     error: state.stocks.error || null,
+//   }),
+//   { loadStocksRequest }
+// )(Stocks);
